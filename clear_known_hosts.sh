a2b81ea43e589830bd6f1cfbcaea684e793a3ea6
Павел Хат <pavelhat@gmail.com>
2022-10-18 20:03:54 +0500

    Add script for clearing known_hosts

---
+++ clear_known_hosts.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+known_hosts=("quoterback.ru" "192.168.10.20" "192.168.10.21")
+
+for host in ${known_hosts[@]}
+do
+	ssh-keygen -f "/home/quotermain/.ssh/known_hosts" -R $host
+done
